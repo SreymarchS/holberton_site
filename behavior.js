284547087a607decdf57612e5c9b8dfcf3ff6047
Sreymarch Soth <sreymarch.soth@gmail.com>
2025-03-09 22:10:45 +1100

    Fix up header and footer

--- behavior.js
+++ behavior.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     var thumbnailElement = document.getElementById("smart_thumbnail");
-
     thumbnailElement.addEventListener("click", function () {
         if (thumbnailElement.className == "") {
             thumbnailElement.className = "small";
@@ -10,3 +9,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(link => {
+        console.log('navlink')
+        if (window.location.href.includes(link.href)) {
+            link.classList.add('active');
+        }
+    });
+});
